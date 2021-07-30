78b97364731f3eb9239b6605184102b9b6ad230d
argelomnes <argelomnes@gmail.com>
2021-07-30 10:59:26 +0800

    Update custom font setting the right way

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    mode: "jit",
-    purge: ["./index.html", "./js/script.js"],
+    mode: 'jit',
+    purge: ['./index.html', './js/script.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             screens: {
-                xs: "365px",
-                lg: "960px",
-                xl: "1160px",
+                xs: '365px',
+                lg: '960px',
+                xl: '1160px',
             },
             fontFamily: {
-                headline: ["Josefin Sans", "sans-serif"],
-                body: ["Alata", "sans-serif"],
+                headline: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+                sans: ['Alata', ...defaultTheme.fontFamily.sans],
             },
             fontSize: {
-                base: ["0.9375rem", { lineHeight: "1.65" }],
+                base: ['0.9375rem', { lineHeight: '1.65' }],
             },
             colors: {
                 teal: colors.teal,
                 gray: colors.trueGray,
-                "dark-gray": "hsl(0, 0%, 55%)",
-                "very-dark-gray": "hsl(0, 0%, 41%)",
+                'dark-gray': 'hsl(0, 0%, 55%)',
+                'very-dark-gray': 'hsl(0, 0%, 41%)',
             },
         },
     },
